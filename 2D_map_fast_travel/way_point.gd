704f18c9083d8618053fa00c71f88784e1de658e
DevDrache <dominik.mast.11@gmail.com>
2024-10-21 18:43:44 +0300

    Add files via upload

---
+++ 2D_map_fast_travel/way_point.gd
@@ -0,0 +1,25 @@
+extends Control
+
+signal pressed(title, sceneNumber)
+
+@export var title: String
+@export var sceneNumber: int
+
+func _ready() -> void:
+	add_to_group("map_way_points")
+	tooltip_text = "test"
+
+func _make_custom_tooltip(_for_text) -> Object:
+	var theme = Theme.new()
+	theme.set_stylebox("panel", "TooltipPanel", StyleBoxEmpty.new())
+	set_theme(theme)
+	
+	#var newLabel = preload("res://customLabel.tscn").instantiate()
+	var newLabel = Label.new()
+	newLabel.text = title
+	newLabel.add_theme_color_override("font_color", Color(0,0,0))
+	
+	return newLabel
+
+func _on_button_pressed() -> void:
+	pressed.emit(title, sceneNumber)
