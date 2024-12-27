983099b162b3f9e1bc68ddff4f837758fefd5314
DevDrache <dominik.mast.11@gmail.com>
2024-12-27 06:58:40 +0200

    Add files via upload

---
+++ UI/map_fast_travel/scene.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var onFastTravel = false
+@export var spawnPoint: Node2D
+@export var fastTravelSpawnPoint : Node2D
+
+const FOX_PLAYER_MAP = preload("res://fox_player_Map.tscn")
+
+func _ready() -> void:
+	var player = FOX_PLAYER_MAP.instantiate()
+	add_child(player)
+
+	if onFastTravel: player.global_position = fastTravelSpawnPoint.global_position
+	else: player.global_position = spawnPoint.global_position
