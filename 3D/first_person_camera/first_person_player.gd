874545bd504d3613b81a3138f0936d105680a39b
DevDrache <dominik.mast.11@gmail.com>
2025-01-06 23:41:02 +0200

    Update first_person_player.gd

--- 3D/first_person_camera/first_person_player.gd
+++ 3D/first_person_camera/first_person_player.gd
@@ -12,7 +12,7 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _input(event):
-	if event.is_action_pressed("quit"): get_tree().quit()
+	if event.is_action_pressed("ui_cancel"): get_tree().quit()
 	
 	if event is InputEventMouseMotion:
 		rotation.y -= event.relative.x * CAMERA_SENS
