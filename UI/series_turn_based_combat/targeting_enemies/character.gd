dd0d1d9305b89214892607e70fa998462e5be579
DevDrache <dominik.mast.11@gmail.com>
2025-01-11 07:40:33 +0200

    Add files via upload

---
+++ UI/series_turn_based_combat/targeting_enemies/character.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+
+@onready var turn_based_agent: TurnBasedAgent = $TurnBasedAgent
+
+func _ready() -> void:
+	turn_based_agent.target_selected.connect(_on_target_selected)
+	
+func _on_target_selected(target: TurnBasedAgent, command: Resource):
+	# here you put every interaction between the character and his targets
+	# like:
+	# animation
+	# damage/heal/buffs
+	# interaction with Hp Bars
+	# and more
+	
+	_animation_example(target)
+
+func _animation_example(target):
+	var startPosition = global_position
+	var targetPosition = target.get_global_position()
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position", targetPosition, 0.5)
+	tween.tween_property(self, "global_position", startPosition, 0.5)
+	
+	await tween.finished
+	
+	turn_based_agent.command_done()
---
+++ UI/series_turn_based_combat/targeting_enemies/skillResource.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name SkillResource
+
+@export var name: String
+@export var targetType: Target_Type
+
+enum Target_Type{ENEMIES, PLAYERS}
