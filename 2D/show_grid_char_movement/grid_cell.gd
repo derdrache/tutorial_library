447b2fdeade817b98e5b11f2d36edc7711d294fe
DevDrache <dominik.mast.11@gmail.com>
2025-09-25 17:38:00 +0300

    Update grid_cell.gd

--- 2D/show_grid_char_movement/grid_cell.gd
+++ 2D/show_grid_char_movement/grid_cell.gd
@@ -1,16 +1,22 @@
 extends PanelContainer
 
+var isSelectable = false
+
 func highlight_cell():
 	var styleBox := get_theme_stylebox("panel").duplicate()
 	styleBox.bg_color = Color(1,1,0,0.5)
 	add_theme_stylebox_override("panel",styleBox)
 
-func select_cell():
+func select_cell():	
+	isSelectable = true
+	
 	var styleBox := get_theme_stylebox("panel").duplicate()
 	styleBox.bg_color = Color(1,1,1, 0.5)
 	add_theme_stylebox_override("panel",styleBox)
 
 func deselect_cell():
+	isSelectable = false
+
 	var styleBox := get_theme_stylebox("panel").duplicate()
 	styleBox.bg_color = Color(0.5,0.5,0.5, 0.5)
 	add_theme_stylebox_override("panel",styleBox)	
