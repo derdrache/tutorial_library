3c92d604db2df0f5249b90e07ef19dd461df13d5
DevDrache <dominik.mast.11@gmail.com>
2025-09-29 06:59:31 +0300

    Add files via upload

---
+++ 3D/create_grapple_gun/gun.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+@onready var bullet_marker: Marker3D = %bulletMarker
+
+const LINE = preload("res://line.tscn")
+
+var line: Node3D
+
+func _process(delta: float) -> void:
+	if line:
+		line.global_position = bullet_marker.global_position
+
+func shoot(targetPosition):
+	_create_line(targetPosition)
+	
+func _create_line(targetPosition):
+	line = LINE.instantiate()
+
+	get_tree().current_scene.add_child(line)
+	line.global_position = bullet_marker.global_position
+	
+	line.set_target_position(targetPosition)
+
+func remove_line():
+	line.queue_free()
+	line = null
