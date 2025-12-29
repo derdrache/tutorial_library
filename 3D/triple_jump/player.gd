adc5ee5b6a1247a4a8759cea1919ac5436cf52ba
DevDrache <dominik.mast.11@gmail.com>
2025-12-29 08:04:32 +0200

    Add files via upload

---
+++ 3D/triple_jump/player.gd
@@ -0,0 +1,60 @@
+extends CharacterBody3D
+
+@onready var model: Node3D = %Model
+@onready var animation_player: AnimationPlayer = $Model/AnimationPlayer
+@onready var multi_jump_timer: Timer = $multiJumpTimer
+@onready var third_person_camera: Node3D = $thirdPersonCamera
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+var jumpCount = 0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		multi_jump_timer.start()
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		var canThirdJump = jumpCount == 2 and velocity != Vector3.ZERO
+		
+		if jumpCount < 2 or canThirdJump:
+			jumpCount += 1
+		else:
+			jumpCount = 1
+			
+		velocity.y = JUMP_VELOCITY * jumpCount
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (third_person_camera.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		var targetAngle = atan2(direction.x, direction.z)
+		model.rotation.y = lerp_angle(model.rotation.y, targetAngle, 0.1)
+		
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	_set_animation(direction)
+
+	move_and_slide()
+
+func _set_animation(direction):
+	if direction:		
+		animation_player.play("Running_A")
+	else:
+		animation_player.play("Idle")
+
+func _on_multi_jump_timer_timeout() -> void:
+	jumpCount = 0
+	
+	
+	
+	
+	
