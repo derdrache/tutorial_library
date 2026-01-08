de00b3533ffec6986d7c66917025b423336c7672
DevDrache <dominik.mast.11@gmail.com>
2026-01-08 05:33:33 +0200

    add hurted signal and clean code

--- 3D/hit_and_hurt_box/hurt_box.gd
+++ 3D/hit_and_hurt_box/hurt_box.gd
@@ -1,15 +1,18 @@
 extends Area3D
 
+signal hurted()
 signal died()
 
 @onready var damage_indicator: Node3D = $damageIndicator
 
 @export var health:= 50
 
-func take_damage(damage: int):
-	damage_indicator.create_indicator_label(-damage)
+func take_damage(value: int):
+	damage_indicator.create_indicator_label(-value)
 	
-	health -= damage
+	health -= value
+
+	hurted.emit()
 
 	if health <= 0:
 		died.emit()
