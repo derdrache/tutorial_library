8f88085f22f6685b0226f168aaab0a80e6d25317
DevDrache <dominik.mast.11@gmail.com>
2026-02-28 07:18:00 +0200

    Add files via upload

---
+++ 2D/platform_teleport_archer/enemy.gd
@@ -0,0 +1,81 @@
+extends CharacterBody2D
+
+@export var teleporterMarker: Array[Marker2D] = []
+
+@onready var rotation_node: Node2D = $rotationNode
+@onready var animated_sprite_2d: AnimatedSprite2D = $rotationNode/AnimatedSprite2D
+@onready var arrow_start_marker: Marker2D = $rotationNode/arrowStartMarker
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+const ARROW = preload("uid://cmjhgeo8j0pb")
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var isShooting = false
+var shootCount = 0
+
+func _physics_process(_delta: float) -> void:
+	if not isShooting:
+		if shootCount == 2:
+			shootCount = 0
+			_teleport()
+		else:
+			_shoot()
+		
+	_set_animation()
+			
+	if isShooting:
+		return
+		
+	move_and_slide()
+
+func _shoot():
+	isShooting = true
+	shootCount += 1
+	animated_sprite_2d.play("shoot")
+
+func _teleport():	
+	set_physics_process(false)
+	collision_shape_2d.disabled = true
+	hide()
+	
+	await get_tree().create_timer(1).timeout
+	
+	var randomMarker = teleporterMarker.pick_random()
+	global_position = randomMarker.global_position
+	
+	set_physics_process(true)
+	collision_shape_2d.disabled = false
+	show()
+
+func _set_animation():
+	var player = get_tree().get_first_node_in_group("player")
+	
+	if not player: return
+	
+	var playerPosition = player.global_position
+	var direction = global_position.direction_to(playerPosition)
+	
+	if direction.x > 0:
+		rotation_node.scale.x = 1
+	elif direction.x < 0:
+		rotation_node.scale.x = -1
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if animated_sprite_2d.animation == "shoot":
+		if animated_sprite_2d.frame == 6:
+			_create_arrow()
+			
+func _create_arrow():
+	var arrowNode = ARROW.instantiate()
+	
+	var player = get_tree().get_first_node_in_group("player")
+	arrowNode.direction = global_position.direction_to(player.global_position)
+	
+	get_tree().current_scene.add_child(arrowNode)
+	arrowNode.global_position = arrow_start_marker.global_position
+	
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "shoot":
+		isShooting = false
